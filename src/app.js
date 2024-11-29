0006f1a583010d22a2d309011c54a02387fb3680
javigarzon1 <javi.garzon@hotmail.com>
2024-11-29 17:40:06 +0000

    pirmer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during myduring my lunch",
+  "while I was praying"
+];
+
+function aleatoria(cadena) {
+  let a = Math.floor(Math.random() * cadena.length);
+  return cadena[a];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let valorWho = aleatoria(who);
+  let valorAction = aleatoria(action);
+  let valorWhat = aleatoria(what);
+  let valorWhen = aleatoria(when);
+
+  const nuevaExcusa =
+    valorWho + " " + valorAction + " " + valorWhat + " " + valorWhen;
+
+  document.getElementById("excusa").innerHTML = nuevaExcusa;
 };
